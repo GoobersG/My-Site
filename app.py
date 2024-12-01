f0014ea950c61effcbb322fd60ff23f5486c67d9
GoobersG <gavigardin@gmail.com>
2024-12-01 03:16:44 +0200

    clean

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 
-app = Flask(__name__, static_folder='Static', template_folder='Templates')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -13,11 +13,8 @@ def home():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        # Here you can handle the form data submission if necessary
-        # For example: send an email or save the data to a database
         print("Form submitted!")
-    return render_template('contact.html')  # Show form on GET request
+    return render_template('contact.html')
 
 if __name__ == "__main__":
-    # Use host='0.0.0.0' for deployment on cloud platforms like Render
     app.run(host='0.0.0.0', debug=True)
